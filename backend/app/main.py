312bbe49d8d5041b7420ea280bb60255dd451a01
Jaweria-Tahir <jaweriatahir368@gmail.com>
2026-08-11 03:07:25 +0500

    Update deployment configuration

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,11 +10,11 @@ app = FastAPI(
 origins = [
     "http://localhost:5500",
     "http://127.0.0.1:5500",
-    "https://sentiva-multinomial-logistic-regres.vercel.app"
+    "https://sentiva-multinomial-logres.vercel.app"
 ]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
